dd066e2721c3cc68b1b034b3b2d896ead31d3aed
Daria Smirnova <darya.smirn1@yandex.ru>
2025-08-07 19:18:56 +0300

    add getting tasks by id

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -19,8 +20,34 @@ var tasks = []Task{
 
 func main() {
 	r := gin.Default()
-	r.GET("/tasks", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, tasks)
-	})
+	r.GET("/tasks", GetTasks)
+	r.GET("/tasks/:id", GetTaskByID)
 	r.Run()
+}
+
+func GetTasks(c *gin.Context) {
+	c.JSON(http.StatusOK, tasks)
+}
+
+func GetTaskByID(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "invalid id parameter"})
+		return
+	}
+	task, ok := findTaskByID(id)
+	if !ok {
+		c.JSON(http.StatusNotFound, gin.H{"message": "not found"})
+		return
+	}
+	c.JSON(http.StatusOK, task)
+}
+
+func findTaskByID(id int) (Task, bool) {
+	for _, task := range tasks {
+		if task.ID == id {
+			return task, true
+		}
+	}
+	return Task{}, false
 }
