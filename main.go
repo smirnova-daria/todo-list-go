51759253376ea3c4d27da581ff5851a444ffb050
Daria Smirnova <darya.smirn1@yandex.ru>
2025-08-07 16:01:49 +0300

    add route GET /tasks with db in memory

--- main.go
+++ main.go
@@ -6,12 +6,21 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Task struct {
+	ID   int    `json:"id"`
+	Text string `json:"text"`
+	Done bool   `json:"done"`
+}
+
+var tasks = []Task{
+	{ID: 1, Text: "todo number 1", Done: true},
+	{ID: 2, Text: "todo №2", Done: false},
+}
+
 func main() {
 	r := gin.Default()
-	r.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": "Hello",
-		})
+	r.GET("/tasks", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, tasks)
 	})
 	r.Run()
 }
